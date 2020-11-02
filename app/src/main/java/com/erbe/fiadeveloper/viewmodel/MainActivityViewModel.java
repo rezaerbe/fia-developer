ca5d6a5a7b7418fb610cf5ce2b1550b1275dc815
unknown <rezarb88@gmail.com>
2020-11-02 23:31:32 +0700

    base layout

--- app/src/main/java/com/erbe/fiadeveloper/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/erbe/fiadeveloper/viewmodel/MainActivityViewModel.java
@@ -1,4 +1,25 @@
 package com.erbe.fiadeveloper.viewmodel;
 
-public class MainActivityViewModel {
+import androidx.lifecycle.ViewModel;
+
+/**
+ * ViewModel for {@link com.erbe.fiadeveloper.ui.MainActivity}.
+ */
+
+public class MainActivityViewModel extends ViewModel {
+
+    private boolean mIsSigningIn;
+
+    public MainActivityViewModel() {
+        mIsSigningIn = false;
+    }
+
+    public boolean getIsSigningIn() {
+        return mIsSigningIn;
+    }
+
+    public void setIsSigningIn(boolean mIsSigningIn) {
+        this.mIsSigningIn = mIsSigningIn;
+    }
+
 }
